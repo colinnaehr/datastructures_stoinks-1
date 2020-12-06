0cb8262ead8682e794f008f2546afa5b35e6fc00
smehringer <spencermehringer@gmail.com>
2020-12-06 16:12:13 -0500

    initial commit

---
+++ src/backend/App.java
@@ -0,0 +1,20 @@
+package backend;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=AMRN&region=US"))
+                .header("x-rapidapi-key", "c38dd0ab0fmsh23d79e093de2477p1fb600jsnf351e6af676a")
+                .header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
