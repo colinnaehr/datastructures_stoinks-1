c8743e375e7492cd6ee60f53ad29f36eca388046
smehringer <spencermehringer@gmail.com>
2020-12-07 00:45:16 -0500

    stock volume query with priority queue

--- src/backend/BackendController.java
+++ src/backend/BackendController.java
@@ -1,8 +1,12 @@
 package backend;
 
+import javafx.util.Pair;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class BackendController {
@@ -15,17 +19,23 @@ public class BackendController {
     }
 
     public void fetchStocks(){
+        System.out.println("Fetching...");
         data.clear();
         try {
             File tickersFile = new File("./src/backend/tickers.txt");
             Scanner reader = new Scanner(tickersFile);
             while (reader.hasNextLine()) {
                 String ticker = reader.nextLine();
-                Stock stonk = handler.getPrices(ticker);
-                stonk.setTicker(ticker);
-                System.out.println(stonk.getTicker());
-                System.out.println(stonk.getTotalVolume());
-                data.add(stonk);
+                try {
+                    Stock stonk = handler.getPrices(ticker);
+                    stonk.setTicker(ticker);
+                    System.out.println(stonk.getTicker());
+                    System.out.println(stonk.getTotalVolume());
+                    data.add(stonk);
+                } catch (Exception e){
+                    System.out.println("Error");
+                }
+
             }
             reader.close();
         } catch (FileNotFoundException e) {
@@ -34,9 +44,30 @@ public class BackendController {
         }
     }
 
-//    public ArrayList<Pair<String,Long>> volumeQuery(){
-//        ArrayList<Pair<String,Long>> results;
-//
-//    }
+    public ArrayList<Pair<String,Long>> volumeQuery(boolean priorityQueue){
+        ArrayList<Pair<String,Long>> results = new ArrayList<>();
+        if (priorityQueue){
+            PriorityQueue<Stock> pq = new PriorityQueue<Stock>(new StockComparator());
+            pq.addAll(data);
+            for (int i = 0; i < 5; i++){
+                Stock top = pq.peek();
+                assert top != null;
+                results.add(new Pair<>(top.getTicker(),top.getTotalVolume()));
+                pq.remove();
+            }
+            return results;
+        } else {
+            return null;
+        }
+    }
 
+}
+
+class StockComparator implements Comparator<Stock>{
+    @Override
+    public int compare(Stock s1, Stock s2){
+        long s1v = s1.getTotalVolume();
+        long s2v = s2.getTotalVolume();
+        return Long.compare(s2v, s1v);
+    }
 }
--- src/backend/RequestTest.java
+++ src/backend/RequestTest.java
@@ -5,10 +5,11 @@ import java.io.File;
 public class RequestTest {
     public static void main(String[] args) {
         BackendController controller = new BackendController();
-        System.out.println(new File(".").getAbsolutePath());
-        controller.fetchStocks();;
-        System.out.println(controller.data.get(0).getTotalVolume());
-        System.out.println(controller.data.get(1).getTotalVolume());
-        System.out.println(controller.data.get(2).getTotalVolume());
+        controller.fetchStocks();
+        System.out.println("    ");
+        for (int i = 0; i < 5; i++){
+            System.out.println(controller.volumeQuery(true).get(i).getKey());
+        }
+
     }
 }
